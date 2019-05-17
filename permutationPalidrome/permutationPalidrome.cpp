1e325f7f2e50f7cf8fcc381fb412e952e442226f
patrickshyee <patrickshyee@gmail.com>
2019-05-17 12:00:48 -0600

    Setting up the challenge
    I set up the permutation as function and got stuck on the swap function.

--- permutationPalidrome/permutationPalidrome.cpp
+++ permutationPalidrome/permutationPalidrome.cpp
@@ -4,18 +4,39 @@
 #include "pch.h"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+using namespace std;
+
+// swap the values at 2 pointers or position
+void swap(char *x, char *y) {
+	char temp = *x;
+	*x = *y;
+	*y = temp;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// Get the permutation and store it in a temp string.
+void permute(char *a, int start, int end) {
+	int i;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	if (start == end) {
+		cout << a << endl;
+	}
+	else {
+		for (i = start; i <= end; i++) {
+			swap((a++), (a + i));
+			permute(a, start++, end);
+			swap(a++, (a + i));
+		}
+	}
+}
+
+bool solution(string s) {
+	bool palidrome = false;
+	return palidrome;
+}
+
+int main()
+{
+	char str[] = "ABC";
+	permute(str, 0, 2);
+	return 0;
+}
